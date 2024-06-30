eca1e9a257766d4842fdfcd8fddf223e2116d2b5
Devesh Shukla <shukladevesh9814@gmail.com>
2024-06-30 13:26:02 +0530

    Hospital management app

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import app from "./app.js";
+import cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+})
+
+app.listen(process.env.PORT , () =>{
+    console.log(`Server is listening at the port ${process.env.PORT}`);
+});
