508ce311eebad91d6f13645789f0e5740bef42af
Devesh Shukla <shukladevesh9814@gmail.com>
2024-07-02 23:38:23 +0530

    Created Several Routes and message controllers.

--- backend/Controllers/userControllers.js
+++ backend/Controllers/userControllers.js
@@ -2,6 +2,7 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { User } from "../models/userSchema.js";
 import { generateToken } from "../utils/jwtToken.js";
+import cloudinary from "cloudinary";
 
 export const patientRegister = catchAsyncErrors(async (req, res, next) => {
   const {
@@ -87,7 +88,9 @@ export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
   }
   const isRegistered = await User.findOne({ email });
   if (isRegistered) {
-    return next(new ErrorHandler(`${isRegistered.role} with this email already exists!!`));
+    return next(
+      new ErrorHandler(`${isRegistered.role} with this email already exists!!`)
+    );
   }
   const admin = await User.create({
     firstName,
@@ -99,25 +102,122 @@ export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
     dob,
     nic,
     role: "Admin",
-  })
-    res.status(200).json({
-      success:true,
-      message:"New Admin Registered!",
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Admin Registered!",
   });
 });
 
-export const getAllDoctors = catchAsyncErrors(async(req,res,next) =>{
-  const doctors = await User.find({role:"Doctor"});
+export const getAllDoctors = catchAsyncErrors(async (req, res, next) => {
+  const doctors = await User.find({ role: "Doctor" });
   res.status(200).json({
-    success:true,
+    success: true,
     doctors,
   });
-}); 
+});
 
-export const getUserDetails = catchAsyncErrors(async(req,res,next) =>{
+export const getUserDetails = catchAsyncErrors(async (req, res, next) => {
   const user = req.user;
   res.status(200).json({
-    success:true,
+    success: true,
     user,
   });
-}); 
+});
+
+export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("adminToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Admin Logged Out Successfully!",
+    });
+});
+
+export const logoutPatient = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("patientToken", "", {
+      httpOnly: true,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Patient Logged Out Successfully!",
+    });
+});
+
+export const addNewDoctor = catchAsyncErrors(async (req, res, next) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return next(new ErrorHandler("Doctor Avatar Required!", 400));
+  }
+  const { docAvatar } = req.files;
+  const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+  if (!allowedFormats.includes(docAvatar.mimetype)) {
+    return next(new ErrorHandler("File Format Not Supported!", 400));
+  }
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    nic,
+    doctorDepartment,
+  } = req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !password ||
+    !gender ||
+    !dob ||
+    !nic ||
+    !doctorDepartment
+  ) {
+    return next(new ErrorHandler("Please Provide Full Details!", 400));
+  }
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(
+      new ErrorHandler(
+        `${isRegistered.role} already registered with this email`,
+        400
+      )
+    );
+  }
+  const cloudinaryResponse = await cloudinary.uploader.upload(
+    docAvatar.tempFilePath
+  );
+  if(!cloudinaryResponse || cloudinaryResponse.error){
+    console.error("Cloudinary Error:",cloudinaryResponse.error || "Unknown cloudinary error");
+  }
+  const doctor = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    password,
+    gender,
+    dob,
+    nic,
+    doctorDepartment,
+    role: "Doctor",
+    docAvatar:{
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    }
+  });
+  res.status(200).json({
+    success: true,
+    message: "New Doctor Registered!",
+    doctor
+  });
+});
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -4,9 +4,10 @@ export const generateToken = (user, message, statusCode, res) => {
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
+      expires: new Date( 
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
+      httpOnly:true
     })
     .json({
       success: true,
