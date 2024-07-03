6f7234e718c2af6ad629e7c931d8ed588f396bdd
Devesh Shukla <shukladevesh9814@gmail.com>
2024-07-03 19:44:43 +0530

    Created the Appointment Functionality

---
+++ backend/router/appointmentRouter.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import {getAllAppointments, postAppointment}  from '../Controllers/appointmentController.js'
+import {isAdminAuthenticated,isPatientAuthenticated} from '../middlewares/auth.js'
+
+const router = express.Router();
+
+ router.post('/post',isPatientAuthenticated, postAppointment);
+ router.get('/getall',isAdminAuthenticated, getAllAppointments);
+
+
+export default router;
