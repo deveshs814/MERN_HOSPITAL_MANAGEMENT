957cbc312b31c51d653316bec31ea829fd8de206
Devesh Shukla <shukladevesh9814@gmail.com>
2024-07-04 23:51:59 +0530

    Created the login,navbar and register profile.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home.jsx'
@@ -6,15 +6,38 @@ import Appointment from './pages/Appointment.jsx'
 import AboutUs from './pages/AboutUs.jsx'
 import Register from './pages/Register.jsx'
 import Login from './pages/Login.jsx'
-
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar.jsx';
+import { Context } from './main.jsx';
 
 
 const App = () => {
+  const {isAuthenticated,setIsAuthenticated,setUser,} = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
   return (
     <>
     <Router>
+      <Navbar/>
         <Routes>
           <Route path='/' element = {<Home/>}/>
           <Route path='/appointment' element = {<Appointment/>}/>
