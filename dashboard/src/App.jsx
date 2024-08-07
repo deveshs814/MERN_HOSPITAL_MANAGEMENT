b33f6b89798255055d171d88e408cc332a4232cf
Devesh shukla <shukladevesh9814@gmail.com>
2024-08-07 13:23:57 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://mern-hospital-management-kiqh.onrender.com/api/v1/user/admin/me",
+          "https://hms-q7hk.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
