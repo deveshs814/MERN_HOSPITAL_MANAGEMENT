434eadd9094fff7795ba5fa8385e8c8a19b8e67c
Devesh shukla <shukladevesh9814@gmail.com>
2024-08-07 13:26:42 +0530

    Update Doctors.jsx

--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
     const fetchDoctors = async () => {
       try {
         const { data } = await axios.get(
-          "https://mern-hospital-management-kiqh.onrender.com/api/v1/user/doctors",
+          "https://hms-q7hk.onrender.com/api/v1/user/doctors",
           { withCredentials: true }
         );
         setDoctors(data.doctors);
