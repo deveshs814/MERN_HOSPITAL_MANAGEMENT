6fe041664c3fbcf2cd90dcd8f271784759bb021b
Devesh shukla <shukladevesh9814@gmail.com>
2024-08-07 13:24:50 +0530

    Update AddNewAdmin.jsx

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "https://mern-hospital-management-kiqh.onrender.com/api/v1/user/admin/addnew",
+          "https://hms-q7hk.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
