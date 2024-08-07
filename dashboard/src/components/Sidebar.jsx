0f542f5b9ffaad6916faa3e93b159f8079c4e69c
Devesh shukla <shukladevesh9814@gmail.com>
2024-08-07 13:27:39 +0530

    Update Sidebar.jsx

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("https://mern-hospital-management-kiqh.onrender.com/api/v1/user/admin/logout", {
+      .get("https://hms-q7hk.onrender.com/api/v1/user/admin/logout", {
         withCredentials: true,
       })
       .then((res) => {
